cf52738e8ae4b026d7322046340188f4c400904a
davidinovargas <davidinovargas@gmail.com>
2025-07-16 13:01:39 -0300

    Ajuste nos Mapeadores

---
+++ ControleDeBar.Infraestrutura.Orm.csproj/ModuloContato/MapeadorContato.cs
@@ -0,0 +1,33 @@
+﻿using eAgenda.Dominio.ModuloContato;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAgenda.Infraestrutura.Orm.ModuloContato
+{
+    public class MapeadorContato : IEntityTypeConfiguration<Contato>
+    {
+        public void Configure(EntityTypeBuilder<Contato> builder)
+        {
+            builder.Property(c => c.Id)
+                .IsRequired();
+
+            builder.Property(c => c.Nome)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Telefone)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(c => c.Email)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Empresa)
+                .HasMaxLength(100);
+
+            builder.Property(c => c.Cargo)
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ ControleDeBar.Infraestrutura.Orm.csproj/ModuloDespesa/MapeadorFormaPagamento.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAgenda.Infra.Orm.Mapeadores
+{
+    public class FormaPagamentoLookup
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+    public class MapeadorFormaPagamento : IEntityTypeConfiguration<FormaPagamentoLookup>
+    {
+        public void Configure(EntityTypeBuilder<FormaPagamentoLookup> builder)
+        {
+            builder.Property("TBFormaPagamento");
+
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Nome)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasData(
+                new FormaPagamentoLookup { Id = 0, Nome = "Dinheiro" },
+                new FormaPagamentoLookup { Id = 1, Nome = "Cartao" },
+                new FormaPagamentoLookup { Id = 2, Nome = "Pix" }
+            );
+        }
+    }
+}
